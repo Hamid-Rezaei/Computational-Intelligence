590ea2acd76b1665708a1374ece12a2bdfa35a2c
hamid <hamidrezaei.ce@gmail.com>
2023-12-24 22:59:27 +0330

    feat: complete layers

--- neural_network/rsdl/layers/fully_connected.py
+++ neural_network/rsdl/layers/fully_connected.py
@@ -1,38 +1,39 @@
 from rsdl import Tensor
 from rsdl.layers import initializer
 
+
 class Linear:
 
     def __init__(self, in_channels, out_channels, need_bias=True, mode='xavier') -> None:
         # set input and output shape of layer
         self.shape = (in_channels, out_channels)
         self.need_bias = need_bias
-        # TODO initialize weight by initializer function (mode)
+        # initialize weight by initializer function (mode)
         self.weight = Tensor(
-            data=None,
-            requires_grad=None
+            data=initializer(shape=self.shape, mode=mode),
+            requires_grad=True
         )
-        # TODO initialize weight by initializer function (zero mode)
+        # initialize weight by initializer function (zero mode)
         if self.need_bias:
             self.bias = Tensor(
-                data=None,
-                requires_grad=None
+                data=initializer(shape=self.shape, mode="zero"),
+                requires_grad=True
             )
 
     def forward(self, inp):
-        # TODO:implement forward propagation
-        return None
-    
+        # forward propagation
+        return inp @ self.weight + self.bias
+
     def parameters(self):
-        
+
         if self.need_bias:
-            return [self. weight, self.bias]
-        return [self. weight]
-    
+            return [self.weight, self.bias]
+        return [self.weight]
+
     def zero_grad(self):
         self.weight.zero_grad()
         if self.need_bias:
             self.bias.zero_grad()
-            
+
     def __call__(self, inp):
         return self.forward(inp)
--- neural_network/rsdl/layers/init.py
+++ neural_network/rsdl/layers/init.py
@@ -4,17 +4,20 @@ import numpy as np
 # TODO: implement xavier_initializer, zero_initializer
 
 def xavier_initializer(shape):
-    return np.random.randn(*shape) * np.sqrt(1/shape[0], dtype=np.float64)
+    return np.random.randn(*shape) * np.sqrt(1 / shape[0], dtype=np.float64)
+
 
 def he_initializer(shape):
-    return np.random.randn(*shape) * np.sqrt(2/shape[0], dtype=np.float64)
+    return np.random.randn(*shape) * np.sqrt(2 / shape[0], dtype=np.float64)
 
 
 def zero_initializer(shape):
     return np.zeros(shape, dtype=np.float64)
 
+
 def one_initializer(shape):
     return np.ones(shape, dtype=np.float64)
+
 
 def initializer(shape, mode="xavier"):
     if mode == "xavier":
