bb9b4e8816350a5b99c0590631d1f9fee8de2a14
hamid <hamidrezaei.ce@gmail.com>
2023-12-24 23:57:10 +0330

    feat: complete losses

--- neural_network/rsdl/losses/loss_functions.py
+++ neural_network/rsdl/losses/loss_functions.py
@@ -1,12 +1,21 @@
+import numpy as np
+
 from rsdl import Tensor
 
+
 def MeanSquaredError(preds: Tensor, actual: Tensor):
-    # TODO : implement mean squared error
-    return None
+    # implement mean squared error
+    E = preds.data - actual.data
+    mse = (1 / len(actual.data)) * np.dot(E.T, E)
+
+    return mse
+
 
 def CategoricalCrossEntropy(preds: Tensor, actual: Tensor):
-    # TODO : imlement categorical cross entropy 
-    return None
+    # implement categorical cross entropy
+    epsilon = 1e-15
+    preds.data = np.clip(preds.data, epsilon, 1 - epsilon)
 
+    ce = -np.sum(actual.data & np.log(preds.data)) / len(actual.data)
 
-
+    return ce
